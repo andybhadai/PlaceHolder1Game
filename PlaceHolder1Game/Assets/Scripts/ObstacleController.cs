b98486d0a24015576a0feb06a7cb8fa6edbf6960
Mark van der Heiden <Marhek@users.noreply.github.com>
2018-09-06 15:42:55 +0200

    Better jumping and placement of obstacles

--- PlaceHolder1Game/Assets/Scripts/ObstacleController.cs
+++ PlaceHolder1Game/Assets/Scripts/ObstacleController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class ObstacleController : MonoBehaviour {
 
-    public GameObject WoodenFence;
-    
+    public GameObject WoodenFence;        
+    public float xPos;
+    public float yPos;
+
     void Start()
     {
         this.SpawnWoodenFence();
@@ -13,7 +15,7 @@ public class ObstacleController : MonoBehaviour {
 
     public void SpawnWoodenFence()
     {
-        this.WoodenFence.transform.position = new Vector3 { x = -10, y = -4 };
+        this.WoodenFence.transform.position = new Vector3(xPos, yPos, 0);
         Instantiate(this.WoodenFence);
     }
 
