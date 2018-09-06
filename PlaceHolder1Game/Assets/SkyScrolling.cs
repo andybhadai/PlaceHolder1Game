9486e3d448516c3649a101969e96a389f07423a9
Mark van der Heiden <Marhek@users.noreply.github.com>
2018-09-06 11:28:58 +0200

    Added my part to Andy's part

---
+++ PlaceHolder1Game/Assets/SkyScrolling.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyScrolling : MonoBehaviour {
+
+	float distance;
+	
+
+	// Use this for initialization
+	void Start () {
+		distance = GetComponent<RectTransform>().rect.width - 0.1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnBecameInvisible() {
+    	transform.Translate(-distance,0,0);
+    }
+}
