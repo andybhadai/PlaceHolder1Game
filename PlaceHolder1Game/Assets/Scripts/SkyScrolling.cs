f251444620bbbdedc8f8d451396861a4c91e23ed
Mark van der Heiden <Marhek@users.noreply.github.com>
2018-09-06 14:56:57 +0200

    Deleted unused Assets. Added Foreground

---
+++ PlaceHolder1Game/Assets/Scripts/SkyScrolling.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyScrolling : MonoBehaviour {
+
+	float distance;
+	public float speed;
+	public float spacing;
+	
+
+	// Use this for initialization
+	void Start () {
+		distance = GetComponent<RectTransform>().rect.width - 0.1f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Translate (Vector2.left * speed * Time.deltaTime);
+	}
+
+	void OnBecameInvisible() {
+    	transform.Translate(-distance - spacing,0,0);
+    }
+}
