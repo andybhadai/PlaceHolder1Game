5ef3a24a8919a8e334d34deb22bcd2fba3c88655
Mark van der Heiden <Marhek@users.noreply.github.com>
2018-09-11 12:33:19 +0200

    Now there wont be a message that a scene "is loading" or "not loaded".

--- PlaceHolder1Game/Assets/Scripts/EndGameTrigger.cs
+++ PlaceHolder1Game/Assets/Scripts/EndGameTrigger.cs
@@ -5,12 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class EndGameTrigger : MonoBehaviour {
 
+	public GameObject mainGame;
+
+	public float distanceToDeath;
+
 	// Update is called once per frame
 	void FixedUpdate () {
-		
-	}
-
-	void OnBecameInvisible() {
-		SceneManager.LoadSceneAsync("GameOver");
+		if (transform.position.x - mainGame.transform.position.x > distanceToDeath) {
+			SceneManager.LoadScene("GameOver");
+		}
 	}
 }
