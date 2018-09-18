8561491e5d0c23a37ee48d76778bffe70700476a
Mark van der Heiden <Marhek@users.noreply.github.com>
2018-09-18 12:30:00 +0200

    Fixed all Obstacle spawning. Also added breakable Obstacles.

--- PlaceHolder1Game/Assets/Scripts/ObjectInvisible.cs
+++ PlaceHolder1Game/Assets/Scripts/ObjectInvisible.cs
@@ -7,9 +7,49 @@ public class ObjectInvisible : MonoBehaviour {
     public int minimumPosition;
     public int maximumPosition;
     public List<Sprite> sprites;
+    public bool isBreakable;
+    public bool isAirObstacle;
+    public GameObject objects;
+    public GameObject mainGame;
+
+
+    void Start()
+    {
+        RandomSprite();
+    }
+
+    void RandomSprite()
+    {
+    	this.GetComponent<SpriteRenderer>().sprite = sprites[Mathf.FloorToInt(Random.Range(0, sprites.Count))];
+    }
 
     void OnBecameInvisible() {
-        this.GetComponent<SpriteRenderer>().sprite = sprites[Mathf.FloorToInt(Random.Range(0, sprites.Count))];
-        transform.Translate(- Random.Range(this.minimumPosition, this.maximumPosition), 0, 0);
+        if(isAirObstacle)
+        {
+            Debug.Log("Bird");
+            objects.GetComponent<ObstacleController>().MoveAirObstacle(minimumPosition, maximumPosition);
+        } 
+        else if(isBreakable)
+        {
+            objects.GetComponent<ObstacleController>().SpawnObstacle();
+            objects.GetComponent<ObstacleController>().MoveObstacle(minimumPosition, maximumPosition);
+        }
+        else if (objects.GetComponent<ObstacleController>().Breakable())
+        {
+            Debug.Log("Break?");
+            objects.GetComponent<ObstacleController>().SpawnBreakableObstacle();
+            objects.GetComponent<ObstacleController>().MoveBreakableObstacle(minimumPosition, maximumPosition);
+        }
+        else
+        {
+            objects.GetComponent<ObstacleController>().MoveObstacle(minimumPosition, maximumPosition);
+        }
+        
+        RandomSprite();
+    }
+
+    public void Delete()
+    {
+        GameObject.Destroy(this);
     }
 }
--- PlaceHolder1Game/Assets/Scripts/ObstacleController.cs
+++ PlaceHolder1Game/Assets/Scripts/ObstacleController.cs
@@ -10,6 +10,10 @@ public class ObstacleController : MonoBehaviour {
     public GameObject airObstacle;
     public float xPosAir;
     public float yPosAir;
+    public GameObject breakableObstacle;
+    public int breakableChance;
+    public GameObject mainGame;
+
 
     void Start()
     {
@@ -19,13 +23,40 @@ public class ObstacleController : MonoBehaviour {
 
     public void SpawnObstacle()
     {
-        obstacle.transform.position = new Vector3(xPos, yPos, 0);
-        Instantiate(obstacle);
+        // obstacle.transform.position = new Vector3(xPos, yPos, 0);
     }
 
     public void SpawnAirObstacle()
     {
-        airObstacle.transform.position = new Vector3(xPosAir, yPosAir, 0);
-        Instantiate(airObstacle);
+        // airObstacle.transform.position = new Vector3(xPosAir, yPosAir, 0);
+    }
+
+    public void SpawnBreakableObstacle()
+    {
+        // breakableObstacle.transform.position = new Vector3(xPos, yPos, 0);
+    }
+
+    public bool Breakable()
+    {   
+        Debug.Log("test");
+        return Random.Range(0, 100) <= breakableChance;
+    }
+
+    public void MoveObstacle(float min, float max)
+    {
+        obstacle.transform.position = new Vector3(mainGame.transform.position.x - Random.Range(min, max), obstacle.transform.position.y, obstacle.transform.position.z);
+        // transform.Translate(- Random.Range(this.minimumPosition, this.maximumPosition), 0, 0);
+    }
+
+    public void MoveAirObstacle(float min, float max)
+    {
+        Debug.Log("Old: "+ airObstacle.transform.position);
+        airObstacle.transform.position = new Vector3(mainGame.transform.position.x - Random.Range(min, max), airObstacle.transform.position.y, airObstacle.transform.position.z);
+        Debug.Log("New: "+ airObstacle.transform.position);
+    }
+
+    public void MoveBreakableObstacle(float min, float max)
+    {
+        breakableObstacle.transform.position = new Vector3(mainGame.transform.position.x - Random.Range(min, max), breakableObstacle.transform.position.y, breakableObstacle.transform.position.z);
     }
 }
