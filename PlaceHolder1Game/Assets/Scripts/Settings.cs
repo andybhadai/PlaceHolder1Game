7f698fb2b17c8b89f58654e8989b855ea621595c
Andy Bhadai <abhadai@gmail.com>
2018-09-18 11:00:26 +0200

    Sound level is saved

---
+++ PlaceHolder1Game/Assets/Scripts/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.UI;
+
+public class Settings : MonoBehaviour {
+
+	public Slider Slider;
+
+	// Use this for initialization
+	void Start () {
+		this.Slider.value = PlayerPrefs.GetFloat("soundLevel", 0f);
+	}
+
+	public void SetSoundLevel(){
+		PlayerPrefs.SetFloat("soundLevel", this.Slider.value);
+	}
+}
