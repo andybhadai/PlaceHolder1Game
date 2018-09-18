1c7464b5449760f86e01c8a660bcca364a3a4753
Andy Bhadai <abhadai@gmail.com>
2018-09-18 13:43:21 +0200

    Added music

---
+++ PlaceHolder1Game/Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour {
+
+	public AudioSource AudioData;
+
+	// Use this for initialization
+	void Start () {
+		this.AudioData = GetComponent<AudioSource>();
+		this.AudioData.loop = true;
+		this.AudioData.Play(0);
+	}
+}
