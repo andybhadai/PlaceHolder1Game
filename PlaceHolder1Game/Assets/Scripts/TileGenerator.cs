3c12912c088fcd4ab17f31bffe9cdc5997ea6396
Andy Bhadai <abhadai@gmail.com>
2018-09-24 15:45:35 +0200

    Made tiles

---
+++ PlaceHolder1Game/Assets/Scripts/TileGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour {
+
+    public GameObject[] GameObjects;
+    private int iterations = 0;
+    public GameObject mainGame;
+    public int chance;
+    private int generatedTiles;
+
+    private int RandomNumber()
+    {
+        return Random.Range(1, GameObjects.Length);
+    }
+
+    void Update()
+    {
+        if(iterations == 50)
+        {
+            generatedTiles++;
+
+            int random = Random.Range(0, 100);
+
+            if(random < chance)
+            {
+                Spawn(RandomNumber());
+            }
+            else
+            {
+                Spawn(0);
+            }
+
+            iterations = 0;
+            
+        }
+        else
+        {
+            iterations++;
+        }
+    }
+
+    private void Spawn(int obstacle)
+    {
+        int x = -10 * generatedTiles;
+        Instantiate(GameObjects[obstacle], new Vector3(mainGame.transform.position.x + x, 0, 0), Quaternion.identity);
+    }
+}
