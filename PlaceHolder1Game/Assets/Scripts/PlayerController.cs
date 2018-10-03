9d43c2361a757e9817bc675ea2a35af8069b8a0c
Mark van der Heiden <Marhek@users.noreply.github.com>
2018-10-03 11:54:46 +0200

    Cleaned up the whole project. When you collect a Pickup it now gets removed.

--- PlaceHolder1Game/Assets/Scripts/PlayerController.cs
+++ PlaceHolder1Game/Assets/Scripts/PlayerController.cs
@@ -295,11 +295,14 @@ public class PlayerController : MonoBehaviour
             
         } else if (Collision.transform.gameObject.tag == "GasBig") {
             this.GetComponent<GasFill>().MaxGas();
+            Collision.gameObject.SetActive(false);
         } else if (Collision.transform.gameObject.tag == "GasSmall") {
             this.GetComponent<GasFill>().FillGas();
+            Collision.gameObject.SetActive(false);
         } else if (Collision.transform.gameObject.tag == "Coin") {
             amountOfCoins++;
             Debug.Log("OOHHHH, YEAHH, COINS!");
+            Collision.gameObject.SetActive(false);
         }
         else {
             PlayerPrefs.SetInt("amountOfCoins", amountOfCoins);
