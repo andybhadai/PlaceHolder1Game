891c351d3af721c083b6b87b0452ec71ab97e5a7
Mark van der Heiden <Marhek@users.noreply.github.com>
2018-10-08 16:52:47 +0200

    Added intro and Nails Animations

---
+++ PlaceHolder1Game/Assets/Scripts/Intro.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Intro : MonoBehaviour {
+
+	public Sprite []sprites;
+	private int counter;
+
+	// Use this for initialization
+	void Start () {
+		if (PlayerPrefs.GetInt("FinishedIntro", 0) == 0){
+			SetSprite();
+		}
+		else
+		{
+			LeaveIntro();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckClick();
+	}
+
+	void SetSprite()
+	{
+		this.GetComponent<SpriteRenderer>().sprite = sprites[counter];
+	}
+
+	public void NextSprite()
+	{
+		if(counter < sprites.Length-1)
+		{
+			counter++;
+			SetSprite();
+		}
+		else
+		{
+			LeaveIntro();
+		}
+	}
+
+	private void CheckClick()
+    {
+        if (Input.touchCount == 1)
+        {
+			Touch touch = Input.GetTouch(0);
+			if(touch.phase == TouchPhase.Ended)
+			{
+				NextSprite();
+			}
+		}
+	}
+
+	void LeaveIntro()
+	{
+		PlayerPrefs.SetInt("FinishedIntro", 1);
+		SceneManager.LoadScene("StartMenu");
+	}
+}
---
+++ PlaceHolder1Game/Assets/Scripts/LoadIntro.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadIntro : MonoBehaviour {
+
+	public bool resetIntro;
+
+	// Use this for initialization
+	void Start () {
+		if(resetIntro)
+		{
+			PlayerPrefs.SetInt("FinishedIntro", 0);
+		}
+		
+		if (PlayerPrefs.GetInt("FinishedIntro", 0) == 0){
+			SceneManager.LoadScene("Intro");
+		}
+	}
+}
